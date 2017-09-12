72cd44b9a8e4b67ab71f7218baf897d1aec7c823
Jon Gunderson <jongund@illinois.edu>
2017-09-11 20:36:56 -0500

    update wsgi file to help find settings

--- pws2/wsgi.py
+++ pws2/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
-
+import sys,os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pws2.settings")
+sys.path.append(os.path.abspath('..'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pws2.settings')
 
 application = get_wsgi_application()
