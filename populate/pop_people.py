6c2f5810610b952a52fb6a0a6d1c9b5cab4a8cb5
Jon Gunderson <jongund@illinois.edu>
2017-09-17 12:21:39 -0500

    updated tracy picutre and bio

--- populate/pop_people.py
+++ populate/pop_people.py
@@ -34,8 +34,10 @@ p.order = 1
 p.title = 'Speech and Language Pathologist'
 p.picture = "img/tracy.png"
 p.description = """
-My inspiration to open Prairie Wind Speech Therapy, Inc. evolved from the requests and dreams of the various families I’ve had the pleasure to collaborate with. I have provided therapy services to both children and adults for over 20 years in many different types of settings, including client homes, community based sites, and school and clinic settings. From early on in my career, my passion has been in working with individuals’ with severe speech impairments and supplementing communication with low and high tech augmentative communication. In addition, I’ve consistently worked with and enjoyed early communicators in the toddler through preschool years.
-Most recently I provided clinical instruction and supervision to undergraduate and graduate students at the University of Illinois Department of Speech and Hearing Science. My specialty areas include parent/toddler groups, individuals using low and high tech augmentative communication and children with speech, language and literacy needs in preschool through elementary school."""
+Tracy is co-owner of Prairie Wind Speech Therapy, Inc.  Tracy received her Masters from the University of Wisconsin-Madison.  She practiced at the Munroe-Meyer institute of the University of Nebraska Omaha and at the Waisman Center of the University of Wisconsin – Madison, prior to moving to Champaign, IL.  Her practice has included parent collaboration and therapy with young children through young adults.  She particularly specializes in autism, apraxia of speech, motor speech, stuttering and receptive / expressive language.  Tracy is Hanen certified (More Than Words) and has received extensive continuing education in social cognitive models from Social Thinking (Michelle Garcia Winner), as well as attending extensive workshops in autism and apraxia of speech.  
+Tracy’s style of therapy draws from the Hanen approach and social thinking models from Michelle Garcia Winner. She uses a multimodal therapy format that includes visual supports, picture based communication systems and sensory integration techniques (tactile, oral and vestibular) in a play based format.  Tracy deeply values partnering with the parent or care provider so that the intervention is functional for the child.   
+Tracy enjoys spending time with friends and family.  Her husband, Jon and her, like taking walks and being outdoors.
+""" 
 p.save()
    
 p = create_person('Cheryl', 'Yepez', 'M.A., CCC-SLP')
