77124b97de712123d36d3242ca42f80cb57d6861
Jon Gunderson <jongund@illinois.edu>
2017-09-17 05:59:21 -0500

    fixed issues with static files

--- pws2/settings.py
+++ pws2/settings.py
@@ -113,4 +113,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
 from pws2.settings_local import *
